a3cbb292ed0449e3157cbc53c1ad59d63075e814
nebula <karlo.tsutskiridze@gmail.com>
2025-03-11 22:02:29 +0400

    added bsf to find degree of separation

--- util.py
+++ util.py
@@ -1,38 +1,28 @@
 class Node():
-    def __init__(self, state, parent, action):
-        self.state = state
-        self.parent = parent
-        self.action = action
-
+  def __init__(self, state, parent, action):
+    self.state = state # current actor
+    self.parent = parent # parent node, indicates the previous actor
+    self.action = action # action taken to reach this state, basically indicates a movie
 
 class StackFrontier():
-    def __init__(self):
-        self.frontier = []
-
-    def add(self, node):
-        self.frontier.append(node)
-
-    def contains_state(self, state):
-        return any(node.state == state for node in self.frontier)
-
-    def empty(self):
-        return len(self.frontier) == 0
-
-    def remove(self):
-        if self.empty():
-            raise Exception("empty frontier")
-        else:
-            node = self.frontier[-1]
-            self.frontier = self.frontier[:-1]
-            return node
-
+  def __init__(self):
+    self.frontier = []
+  def add(self, node): self.frontier.append(node)
+  def contains_state(self, state): return any(node.state == state for node in self.frontier)
+  def empty(self): return len(self.frontier) == 0
+  def remove(self):
+    if self.empty():
+      raise Exception("empty frontier")
+    else:
+      node = self.frontier[-1]
+      self.frontier = self.frontier[:-1]
+      return node
 
 class QueueFrontier(StackFrontier):
-
-    def remove(self):
-        if self.empty():
-            raise Exception("empty frontier")
-        else:
-            node = self.frontier[0]
-            self.frontier = self.frontier[1:]
-            return node
+  def remove(self):
+    if self.empty():
+      raise Exception("empty frontier")
+    else:
+      node = self.frontier[0]
+      self.frontier = self.frontier[1:]
+      return node
